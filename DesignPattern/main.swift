593b6dd5c87870ea1c911207ed98a8875cd6a5ab
MeePwn <maybewaityou@gmail.com>
2016-06-30 14:12:59 +0800

    Initial Commit

---
+++ DesignPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DesignPattern
+//
+//  Created by ChunNan on 16/6/30.
+//  Copyright © 2016年 MeePwn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
