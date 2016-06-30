dfbb5fb856485e6ed6389c2367ebb5a754485eda
MeePwn <maybewaityou@gmail.com>
2016-06-30 14:29:13 +0800

    add Singleton

---
+++ DesignPattern/Singleton/Singleton.swift
@@ -0,0 +1,17 @@
+//
+//  Singleton.swift
+//  DesignPattern
+//
+//  Created by ChunNan on 16/6/30.
+//  Copyright © 2016年 MeePwn. All rights reserved.
+//
+
+import Foundation
+
+class Singleton {
+    
+    static let shareInstance = Singleton()
+    
+    private init() {}
+    
+}
--- DesignPattern/main.swift
+++ DesignPattern/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+/**
+ 单例模式
+ */
+func singleton() {
+    let a = Singleton.shareInstance
+    let b = Singleton.shareInstance
+    
+    let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    
+    print("=====>>>> \(unsafeAddressOf(a))")
+    print("=====>>>> \(unsafeAddressOf(b))")
+    
+    dispatch_async(queue) {
+        let c = Singleton.shareInstance
+        let d = Singleton.shareInstance
+        let e = Singleton.shareInstance
+        print("=====>>>> \(unsafeAddressOf(c))")
+        print("=====>>>> \(unsafeAddressOf(d))")
+        print("=====>>>> \(unsafeAddressOf(e))")
+        
+    }
+    
+    NSRunLoop.currentRunLoop().run()
+}
